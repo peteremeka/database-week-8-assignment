d05bea08c5e7fdc34eb8e7c5ff52086cc19b59f3
peter emeka <peteremeka51@gmail.com>
2025-09-26 09:59:33 +0100

    made some changes

--- library_mgt_db.sql
+++ library_mgt_db.sql
@@ -13,7 +13,8 @@ CREATE TABLE admins (
   email varchar(255) NOT NULL,
   username varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
-  picture varchar(255) NOT NULL
+  picture varchar(255) NOT NULL,
+  PRIMARY KEY(admin_id)
 )
 
 
@@ -30,7 +31,8 @@ CREATE TABLE borrowers (
   users varchar(255) NOT NULL,
   title varchar(255) NOT NULL,
   edition_id int(10) DEFAULT NULL,
-  dates date NOT NULL
+  dates date NOT NULL,
+  PRIMARY KEY(borrow_id)
 )
 
 -- Table structure for table departments
@@ -38,14 +40,16 @@ CREATE TABLE borrowers (
 CREATE TABLE departments (
   dept_id int(10) NOT NULL,
   dept_name varchar(255) NOT NULL,
-  dept varchar(255) NOT NULL
+  dept varchar(255) NOT NULL,
+  PRIMARY KEY(dept_id)
 ) 
 
 -- Table structure for table editions
 
 CREATE TABLE editions (
   edition_id int(10) NOT NULL,
-  editions varchar(255) DEFAULT NULL
+  editions varchar(255) DEFAULT NULL,
+  PRIMARY KEY(edition_id)
 )
 
 -- Table structure for table projects
@@ -61,14 +65,16 @@ CREATE TABLE projects (
   content_reader int(10) NOT NULL,
   design_reader int(10) NOT NULL,
   month_year varchar(255) NOT NULL,
-  files varchar(255) NOT NULL
+  files varchar(255) NOT NULL,
+  PRIMARY KEY(project_id)
 ) 
 
 -- Table structure for table sessions
 
 CREATE TABLE sessions (
   session_id int(10) NOT NULL,
-  session_name varchar(255) NOT NULL
+  session_name varchar(255) NOT NULL,
+  PRIMARY KEY(session_id)
 ) 
 
 -- Table structure for table statistics
@@ -81,7 +87,8 @@ CREATE TABLE statistics (
   borrows int(10) NOT NULL,
   male int(10) NOT NULL,
   female int(10) NOT NULL,
-  session_id int(10) NOT NULL
+  session_id int(10) NOT NULL,
+  PRIMARY KEY(stat_id)
 ) 
 
 -- Table structure for table textbooks
@@ -98,7 +105,8 @@ CREATE TABLE textbooks (
   call_number varchar(255) NOT NULL,
   stack varchar(255) NOT NULL,
   files varchar(255) NOT NULL,
-  picture varchar(255) NOT NULL
+  picture varchar(255) NOT NULL,
+  PRIMARY KEY(book_id)
 ) 
 
 -- Table structure for table users
@@ -116,118 +124,14 @@ CREATE TABLE users (
   email varchar(255) NOT NULL,
   password varchar(255) NOT NULL,
   picture varchar(255) DEFAULT NULL,
-  register varchar(255) DEFAULT NULL
+  register varchar(255) DEFAULT NULL,
+  PRIMARY KEY(user_id)
 )
 
 
---
--- Indexes for table borrowers
---
-ALTER TABLE borrowers
-  ADD PRIMARY KEY (borrow_id),
-  ADD KEY FK_dept_id (dept_id);
-
---
--- Indexes for table departments
---
-ALTER TABLE departments
-  ADD PRIMARY KEY (dept_id);
-
---
--- Indexes for table editions
---
-ALTER TABLE editions
-  ADD PRIMARY KEY (edition_id);
-
---
--- Indexes for table projects
---
-ALTER TABLE projects
-  ADD PRIMARY KEY (project_id),
-  ADD KEY FK_pro_dept_id (dept_id);
-
---
--- Indexes for table sessions
---
-ALTER TABLE sessions
-  ADD PRIMARY KEY (session_id);
-
---
--- Indexes for table statistics
---
-ALTER TABLE statistics
-  ADD PRIMARY KEY (stat_id),
-  ADD KEY FK_session_id (session_id);
-
---
--- Indexes for table textbooks
---
-ALTER TABLE textbooks
-  ADD PRIMARY KEY (book_id),
-  ADD KEY dept_id (dept_id),
-  ADD KEY edition_id (edition_id);
-
---
--- Indexes for table users
---
-ALTER TABLE users
-  ADD PRIMARY KEY (user_id),
-  ADD KEY FK_user_dept_id (dept_id);
+-- Constraints for tables
 
 
---
--- AUTO_INCREMENT for table borrowers
---
-ALTER TABLE borrowers
-  MODIFY borrow_id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
-
---
--- AUTO_INCREMENT for table departments
---
-ALTER TABLE departments
-  MODIFY dept_id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table editions
---
-ALTER TABLE editions
-  MODIFY edition_id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table projects
---
-ALTER TABLE projects
-  MODIFY project_id int(10) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table sessions
---
-ALTER TABLE sessions
-  MODIFY session_id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table statistics
---
-ALTER TABLE statistics
-  MODIFY stat_id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table textbooks
---
-ALTER TABLE textbooks
-  MODIFY book_id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table users
---
-ALTER TABLE users
-  MODIFY user_id int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-
---
--- Constraints for dumped tables
---
-
---
 -- Constraints for table borrowers
 --
 ALTER TABLE borrowers
@@ -257,8 +161,3 @@ ALTER TABLE textbooks
 --
 ALTER TABLE users
   ADD CONSTRAINT FK_user_dept_id FOREIGN KEY (dept_id) REFERENCES departments (dept_id);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
